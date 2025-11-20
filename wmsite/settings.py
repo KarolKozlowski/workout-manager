ef0350ba5fcc109d356abd5fc0308cc5c0dcbd74
Karol Kozlowski <karol.kozlowski@dotnot.pl>
2025-11-21 00:24:02 +0100

    generate secret on the fly if absent

--- wmsite/settings.py
+++ wmsite/settings.py
@@ -20,15 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() in ("true", "1", "t")
+
 # SECURITY WARNING: keep the secret key used in production secret!
 if os.environ.get("DJANGO_SECRET_KEY"):
     SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 else:
-    from django.core.management.utils import get_random_secret_key;
-    SECRET_KEY = get_random_secret_key()
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() in ("true", "1", "t")
+    if DEBUG:
+        from django.core.management.utils import get_random_secret_key;
+        SECRET_KEY = get_random_secret_key()
+    else:
+        raise ValueError("The DJANGO_SECRET_KEY environment variable is not set.")
 
 ALLOWED_HOSTS = (
     os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
