fcf71290f8ca364b3881efec89bf24cef13ae3ee
Karol Kozlowski <karol.kozlowski@dotnot.pl>
2025-11-21 10:04:20 +0100

    change db location

--- build-n-serve.sh
+++ build-n-serve.sh
@@ -4,5 +4,5 @@ docker run \
   -it \
   -p 8080:80 \
   -e DJANGO_DEBUG=True \
-  -v $(pwd)/db.sqlite3:/app/db.sqlite3 \
+  -v $(pwd)/db/db.sqlite3:/app/db/db.sqlite3 \
   workout-manager
--- wmsite/settings.py
+++ wmsite/settings.py
@@ -84,10 +84,13 @@ WSGI_APPLICATION = "wmsite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+db_path = BASE_DIR / "db"
+db_path.mkdir(exist_ok=True)
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db" / "db.sqlite3",
     }
 }
 
