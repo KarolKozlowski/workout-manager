288631e5ec6d224ce42837bd05ff39893a938542
Karol Kozlowski <karol.kozlowski@dotnot.pl>
2025-11-21 10:03:40 +0100

    use debug when running locally

--- build-n-serve.sh
+++ build-n-serve.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build . \
-  -t workout-manager
+docker build -t workout-manager .
 docker run \
   -it \
   -p 8080:80 \
+  -e DJANGO_DEBUG=True \
   -v $(pwd)/db.sqlite3:/app/db.sqlite3 \
   workout-manager
