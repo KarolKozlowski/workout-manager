6937ed4e2108225db20c5926a552ed57565d3803
Karol Kozlowski <karol.kozlowski@dotnot.pl>
2025-11-22 23:00:51 +0100

    add simple workout presentation

--- wmapp/views.py
+++ wmapp/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
+
+from wmapi.models import Workout
 
 
 class IndexView(TemplateView):
@@ -8,3 +11,19 @@ class IndexView(TemplateView):
     """
 
     template_name = "wmapp/index.html"
+
+
+def workout_detail(request, pk):
+    """Load a `Workout` and render a page showing its children.
+
+    The view prefetches related objects for reasonable performance.
+    """
+    qs = Workout.objects.prefetch_related(
+        "workout_parts",
+        "workout_parts__exercises",
+        "workout_parts__exercises__exercise_id",
+        "workout_parts__exercises__sets",
+    )
+    workout = get_object_or_404(qs, pk=pk)
+
+    return render(request, "wmapp/workout_detail.html", {"workout": workout})
